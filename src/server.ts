035aed7a09cfe0e2be96cefef54b80bb142a9d58
rgkip01 <web.nogs@gmail.com>
2023-05-30 14:34:53 -0300

    initial configs

---
+++ src/server.ts
@@ -0,0 +1,8 @@
+import express, { Application } from "express";
+
+const app:Application = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true }));
+
+app.listen(3000, () => console.log('Server is running in port 3000'));
