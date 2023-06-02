71b2d4b14dff7ff2a00c27f998558dcf6aa13786
rgkip01 <web.nogs@gmail.com>
2023-06-01 23:27:14 -0300

    create middleware to routes

---
+++ src/middlewares/AuthMiddleware.ts
@@ -0,0 +1,33 @@
+import { NextFunction, Request, Response } from "express";
+import { verify } from "jsonwebtoken";
+interface IPayload{
+  sub: string;
+}
+
+class AuthMiddleware {
+  auth(request: Request, response: Response, next: NextFunction){
+    const authHeaders = request.headers.authorization
+
+    if(!authHeaders){
+      return response.status(401).json({
+        code: 'token.missing',
+        message: 'Token missing',
+      });
+    }
+    
+    const [, token] = authHeaders.split(' ');
+    let secretKey: string | undefined = process.env.API_SECRET_KEY
+
+    if (!secretKey) {
+      throw new Error("There is no token key");
+    }
+    try {
+      const { sub } = verify(token, secretKey) as IPayload;
+      request.user_id = sub;
+    } catch (error) {
+      next(error)
+    }
+  }
+}
+
+export { AuthMiddleware };
